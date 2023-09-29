ef9fc6a835c76a20bdb78fbf7a870d73adf1f6c4
Miguel-player-09 <121067934+Miguel-player-09@users.noreply.github.com>
2023-09-28 21:59:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function preload () {
+    
+}
+function setup () {
+
+}
+function draw () {
+
+}
+function takeSnapshot () {
+
+}
+function Nariz1 () {
+
+}
+function Nariz2 () {
+
+}
+function Nariz3 () {
+
+}
+function Nariz4 () {
+
+}
+function Nariz5 () {
+
+}
+function removerNariz () {
+
+}
